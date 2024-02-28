e6ee71aa94f7344be29d509ca06bd5c53f9be947
Woohyun Joh <jeremiahjoh@sungkyul.ac.kr>
2024-02-28 16:30:23 +0900

    feat: implement singly linked list

--- src/ll.h
+++ src/ll.h
@@ -7,7 +7,7 @@ typedef int type;
 #include <stdlib.h>
 
 typedef struct {
-    struct sll_node *head, *tail;
+    struct sll_node *root;
     size_t len;
 } sll;
 
@@ -19,57 +19,214 @@ struct sll_node {
 static sll
 sll_new()
 {
-    sll ll = { NULL, NULL, 0 };
+    sll ll = { NULL, 0 };
     return ll;
+}
+
+static sll
+sll_copy(const sll old)
+{
+    struct sll_node *tmp, *cpy_node;
+    sll cpy;
+
+    if (old.root == NULL)
+        return sll_new();
+
+    cpy_node = cpy.root = malloc(sizeof(struct sll_node));
+    for (tmp = old.root; tmp != NULL; tmp = tmp->next) {
+        cpy_node->data = tmp->data;
+        cpy_node->next = tmp->next ? malloc(sizeof(struct sll_node)) : NULL;
+        cpy_node = cpy_node->next;
+    }
+
+    cpy.len = old.len;
+
+    return cpy;
 }
 
 static void
 sll_free(sll *ll)
 {
     struct sll_node *tmp, *next;
-    for (tmp = ll->head; tmp != NULL; tmp = next) {
+
+    for (tmp = ll->root; tmp != NULL; tmp = next) {
         next = tmp->next;
         free(tmp);
     }
 
-    ll->head = ll->tail = NULL;
+    ll->root = NULL;
     ll->len = 0;
 }
 
 static int
-sll_push_back(sll *ll, const type data)
+sll_push(sll *ll, const type data)
 {
-    if (ll->head == NULL && ll->tail == NULL)
-        ll->head = ll->tail = malloc(sizeof(struct sll_node));
-    else
-        ll->tail = ll->tail->next = malloc(sizeof(struct sll_node));
+    struct sll_node *tmp = malloc(sizeof(struct sll_node));
+    tmp->data = data;
+    tmp->next = ll->root;
 
-    if (ll->tail == NULL)
-        return -1;
-
-    ll->tail->data = data;
-    ll->tail->next = NULL;
+    ll->root = tmp;
     ll->len++;
 
     return 0;
 }
 
 static int
-sll_push_front(sll *ll, const type data)
+sll_pop(sll *ll, type *data)
 {
-    struct sll_node *tmp = malloc(sizeof(struct sll_node));
-    if (tmp == NULL)
+    struct sll_node *next;
+    
+    if (ll->root == NULL)
         return -1;
 
-    tmp->next = ll->head;
-    tmp->data = data;
-    ll->len++;
-    
-    if (ll->head == NULL && ll->tail == NULL)
-        ll->tail = tmp;
-    ll->head = tmp;
+    *data = ll->root->data;
+
+    next = ll->root->next;
+    free(ll->root);
+
+    ll->root = next;
+    ll->len--;
 
     return 0;
+}
+
+static int
+sll_insert(sll *ll, const type data, const size_t at)
+{
+    struct sll_node *tmp, *new; 
+    size_t i;
+
+    for (tmp = ll->root, i = 0; i < at - 1; tmp = tmp->next, i++)
+        if (tmp == NULL)
+            return -1;
+
+    new = malloc(sizeof(struct sll_node));
+    new->data = data;
+    new->next = tmp->next;
+    tmp->next = new;
+
+    ll->len++;
+
+    return 0;
+}
+
+static size_t
+sll_search(const sll *ll, const type data)
+{
+    struct sll_node *tmp;
+    size_t i;
+
+    for (tmp = ll->root, i = 0; tmp != NULL; tmp = tmp->next, i++)
+        if (tmp->data == data)
+            break;
+
+    return i;
+}
+
+static size_t
+sll_remove(sll *ll, const type data)
+{
+    struct sll_node *tmp, *prv;
+    size_t i;
+
+    for (tmp = ll->root, i = 0; tmp != NULL; tmp = tmp->next, i++) {
+        if (tmp->data == data)
+            break;
+
+        prv = tmp;
+    }
+
+    if (tmp == NULL)
+        return i;
+
+    prv->next = tmp->next;
+    free(tmp);
+    ll->len--;
+
+    return i;
+}
+
+static int
+sll_getnth(const sll *ll, type *data, const size_t at)
+{
+    struct sll_node *tmp;
+    size_t i;
+
+    for (tmp = ll->root, i = 0; i < at; tmp = tmp->next, i++)
+        if (tmp == NULL)
+            return -1;
+
+    *data = tmp->data;
+
+    return 0;
+}
+
+static int
+sll_setnth(const sll *ll, const type data, const size_t at)
+{
+    struct sll_node *tmp;
+    size_t i;
+
+    for (tmp = ll->root, i = 0; i < at; tmp = tmp->next, i++)
+        if (tmp == NULL)
+            return -1;
+
+    tmp->data = data;
+
+    return 0;
+}
+
+static int
+sll_rmvnth(sll *ll, type *data, const size_t at)
+{
+    struct sll_node *tmp, *prv;
+    size_t i;
+
+    for (tmp = ll->root, i = 0; i < at; tmp = tmp->next, i++) {
+        if (tmp == NULL)
+            return -1;
+
+        prv = tmp;
+    }
+
+    *data = tmp->data;
+
+    prv->next = tmp->next;
+    free(tmp);
+    ll->len--;
+
+    return 0;
+}
+
+static type *
+sll_getptr(const sll *ll, const size_t at)
+{
+    struct sll_node *tmp;
+    size_t i;
+
+    for (tmp = ll->root, i = 0; i < at; tmp = tmp->next, i++)
+        if (tmp == NULL)
+            return NULL;
+
+    return &tmp->data;
+}
+
+static type *
+sll_first(const sll *ll)
+{
+    return &ll->root->data;
+}
+
+static type *
+sll_last(const sll *ll)
+{
+    return sll_getptr(ll, ll->len - 1);
+}
+
+static size_t
+sll_length(const sll *ll)
+{
+    return ll->len;
 }
 
 #endif
--- test.c
+++ test.c
@@ -4,6 +4,16 @@
 #include <string.h>
 
 static void
+test_vec_copy()
+{
+    int arr[] = { 1, 2, 3, 4, 5 };
+    vec old = vec_from_arr(arr, 5);
+    vec cpy = vec_copy(old);
+
+    assert(memcmp(old.arr, cpy.arr, sizeof(int) * old.len) == 0);
+}
+
+static void
 test_vec_slice()
 {
     int arr[] = { 1, 2, 3, 4, 5 };
@@ -71,20 +81,20 @@ test_vec_get_set_rmv()
 }
 
 static void
-test_vec_nth()
+test_vec_getptr()
 {
     vec vec = vec_new();
     int i, tmp;
     for (i = 0; i < 8; i++)
         vec_push_back(&vec, i);
     
-    tmp = *vec_nth(&vec, 3);
+    tmp = *vec_getptr(&vec, 3);
     assert(tmp == 3);
 
-    *vec_front(&vec) = tmp;
+    *vec_first(&vec) = tmp;
     assert(vec.arr[0] == 3);
 
-    tmp = *vec_back(&vec);
+    tmp = *vec_last(&vec);
     assert(tmp == 7);
 }
 
@@ -114,21 +124,113 @@ test_vec_free()
 }
 
 static void
+test_sll_copy()
+{
+    sll ll, copy;
+    size_t i;
+
+    ll = sll_new();
+
+    sll_push(&ll, 0);
+    sll_push(&ll, 1);
+    sll_push(&ll, 2);
+
+    copy = sll_copy(ll);
+
+    for (i = 0; i < sll_length(&ll); i++)
+        assert(*sll_getptr(&ll, i) == *sll_getptr(&copy, i));
+}
+
+static void
 test_sll_push()
 {
     sll ll = sll_new();
-    sll_push_back(&ll, 0);
-    sll_push_back(&ll, 1);
+    sll_push(&ll, 0);
+    sll_push(&ll, 1);
 
-    assert(ll.head->data == 0);
-    assert(ll.head->next->data == 1);
-    assert(ll.tail == ll.head->next);
+    assert(ll.root->data == 1);
+    assert(ll.root->next->data == 0);
+}
+
+static void
+test_sll_pop()
+{
+    sll ll = sll_new();
+    type data;
+
+    sll_push(&ll, 0);
+    sll_push(&ll, 1);
+
+    sll_pop(&ll, &data);
+    assert(ll.root->data == 0);
+    assert(data == 1);
+    assert(sll_length(&ll) == 1);
+}
+
+static void
+test_sll_insert()
+{
+    sll ll = sll_new();
+
+    sll_push(&ll, 0);
+    sll_push(&ll, 1);
+
+    sll_insert(&ll, 2, 1);
+
+    assert(ll.root->data == 1);
+    assert(ll.root->next->data == 2);
+    assert(ll.root->next->next->data == 0);
+    assert(sll_length(&ll) == 3);
+}
+
+static void
+test_sll_search_remove()
+{
+    size_t i;
+    sll ll = sll_new();
+
+    for (i = 0; i < 8; i++)
+        sll_push(&ll, i);
     
-    sll_push_front(&ll, 0);
-    sll_push_front(&ll, 1);
-    
-    assert(ll.head->data == 1);
-    assert(ll.head->next->data == 0);
+    assert(sll_search(&ll, 4) == 3);
+    assert(sll_remove(&ll, 4) == 3);
+    assert(sll_length(&ll) == 7);
+}
+
+static void
+test_sll_get_set_rmv()
+{
+    type data;
+    sll ll = sll_new();
+
+    sll_push(&ll, 0);
+    sll_push(&ll, 1);
+    sll_push(&ll, 2);
+
+    sll_getnth(&ll, &data, 1);
+    assert(data == 1);
+
+    sll_setnth(&ll, data, 0);
+    assert(ll.root->data == data);
+
+    sll_rmvnth(&ll, &data, 2);
+    assert(ll.len == 2);
+    assert(ll.root->next->next == NULL);
+}
+
+static void
+test_sll_getptr()
+{
+    sll ll = sll_new();
+
+    sll_push(&ll, 0);
+    sll_push(&ll, 1);
+
+    assert(*sll_getptr(&ll, 0) == ll.root->data);
+    assert(*sll_getptr(&ll, 1) == ll.root->next->data);
+
+    assert(*sll_first(&ll) == 1);
+    assert(*sll_last(&ll) == 0);
 }
 
 static void
@@ -138,26 +240,33 @@ test_sll_free()
     sll ll = sll_new();
     
     for (i = 0; i < 8; i++)
-        sll_push_back(&ll, i);
+        sll_push(&ll, i);
     
     sll_free(&ll);
 
-    assert(ll.head == NULL && ll.tail == NULL);
-    assert(ll.len == 0);
+    assert(ll.root == NULL);
+    assert(sll_length(&ll) == 0);
 }
 
 int
 main()
 {
+    test_vec_copy();
     test_vec_slice();
     test_vec_push_pop();
     test_vec_insert_search_remove();
     test_vec_get_set_rmv();
-    test_vec_nth();
+    test_vec_getptr();
     test_vec_resize();
     test_vec_free();
 
+    test_sll_copy();
     test_sll_push();
+    test_sll_pop();
+    test_sll_insert();
+    test_sll_search_remove();
+    test_sll_get_set_rmv();
+    test_sll_getptr();
     test_sll_free();
 
     return 0;
