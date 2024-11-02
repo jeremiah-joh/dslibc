fed96e3b8fd1a678384ddd712eb125dd606d79b4
Woohyun Joh <jeremiahjoh@sungkyul.ac.kr>
2024-11-02 15:28:50 +0900

    feat: test free()

--- test/rbt.c
+++ test/rbt.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#define LEN 5
+
 int cmp(int x, int y) { return x - y; }
 
 INIT_RBT_BOTH(int, int, cmp, malloc, free);
@@ -26,10 +28,25 @@ insert(void)
 	assert(rbt.root->kid[1]->kid[1]->val == 4);
 }
 
+static void
+free_t(void)
+{
+	struct rbt_int rbt;
+	int arr[] = { 0, 1, 2, 3, 4 };
+
+	rbt = rbt_int_from(arr, LEN);
+
+	rbt_int_free(&rbt);
+
+	assert(rbt.root == NULL);
+	assert(rbt.len == 0);
+}
+
 int
 main(void)
 {
 	insert();
+	free_t();
 
 	return 0;
 }
