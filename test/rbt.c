afafedb02568b148ab1e6b38f7a3178be5a70f2c
Woohyun Joh <jeremiahjoh@sungkyul.ac.kr>
2024-12-10 23:02:25 +0900

    fix: replace tree_print to verify

--- test/rbt.c
+++ test/rbt.c
@@ -8,20 +8,45 @@ int cmp(int x, int y) { return x - y; }
 INIT_RBT_BOTH(int, int, cmp, malloc, free);
 
 static void
-print_tree(struct rbt_int_node *cur, int indent, int dir)
+verify_red(struct rbt_int_node *cur)
 {
-	int i;
-
 	if (cur == NULL)
 		return;
-	for (i = 0; i < indent - 1; i++)
-		putchar(' ');
+	if (COLOR(cur) == RED) {
+		assert(COLOR(cur->par) == BLACK);
+		assert(COLOR(cur->kid[0]) == BLACK);
+		assert(COLOR(cur->kid[1]) == BLACK);
+	}
 
-	printf("%s", dir ? "└──" : "├──");
-	printf("%2d %s\n", cur->val, (cur->col == RED) ? "RED" : "BLACK");
+	verify_red(cur->kid[0]);
+	verify_red(cur->kid[1]);
+}
 
-	print_tree(cur->kid[0], indent + 4, 0);
-	print_tree(cur->kid[1], indent + 4, 1);
+static void
+verify_num(struct rbt_int_node *cur, int black_n, int *black_path_n)
+{
+	if (COLOR(cur) == BLACK)
+		black_n++;
+	if (cur) {
+		verify_num(cur->kid[0], black_n, black_path_n);
+		verify_num(cur->kid[1], black_n, black_path_n);
+	} else {
+		if (*black_path_n < 0)
+			*black_path_n = black_n;
+		else	
+			assert(black_n == *black_path_n);
+	}
+}
+
+static void
+verify(struct rbt_int *rbt)
+{
+	int black_path_n;
+
+	black_path_n = -1;
+
+	verify_red(rbt->root);
+	verify_num(rbt->root, 0, &black_path_n);
 }
 
 static void
@@ -33,11 +58,10 @@ test_insert(void)
 	srand(16);
 	rbt = rbt_int_new();
 
-	for (i = 0; i < 32; i++) {
+	for (i = 0; i < 32; i++)
 		assert(rbt_int_insert(&rbt, rand() % 100) == 0);
-	}
 
-	print_tree(rbt.root, 0, 1);
+	verify(&rbt);
 }
 
 int
