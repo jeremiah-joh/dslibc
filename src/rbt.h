2992fdf88089b1e7293d693753efcb850e5d0dec
Woohyun Joh <jeremiahjoh@sungkyul.ac.kr>
2024-12-16 23:54:44 +0900

    fix: replace tab to space

--- src/rbt.h
+++ src/rbt.h
@@ -259,25 +259,25 @@ rbt_##name##_insert(struct rbt_##name *rbt, const type val)                    \
 void                                                                           \
 rbt_##name##_free(struct rbt_##name *rbt)                                      \
 {                                                                              \
-	struct rbt_##name##_node *del, *cur, *prv;                             \
+        struct rbt_##name##_node *del, *cur, *prv;                             \
                                                                                \
-	cur = rbt->root;                                                       \
-	prv = NULL;                                                            \
+        cur = rbt->root;                                                       \
+        prv = NULL;                                                            \
                                                                                \
-	while (cur) {                                                          \
-		if (cur->kid[0] && cur->kid[0] != prv) {                       \
-			cur = cur->kid[0];                                     \
-		} else if (cur->kid[1] && cur->kid[1] != prv) {                \
-			cur = cur->kid[1];                                     \
-		} else {                                                       \
-			del = prv = cur;                                       \
-			cur = cur->par;                                        \
-			free(del);                                             \
-		}                                                              \
-	}                                                                      \
+        while (cur) {                                                          \
+                if (cur->kid[0] && cur->kid[0] != prv) {                       \
+                        cur = cur->kid[0];                                     \
+                } else if (cur->kid[1] && cur->kid[1] != prv) {                \
+                        cur = cur->kid[1];                                     \
+                } else {                                                       \
+                        del = prv = cur;                                       \
+                        cur = cur->par;                                        \
+                        free(del);                                             \
+                }                                                              \
+        }                                                                      \
                                                                                \
-	rbt->root = NULL;                                                      \
-	rbt->len = 0;                                                          \
+        rbt->root = NULL;                                                      \
+        rbt->len = 0;                                                          \
 }                                                                              \
                                                                                \
 extern int _rbt_func_##name
